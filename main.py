0ff8487866c233476065db1110572724c3ccd282
Sheheryar942 <132945798+Sheheryar942@users.noreply.github.com>
2023-05-09 00:44:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,33 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# Load data
-data = pd.read_csv('maintenance_data.csv')
+# Create random data for predictive maintenance
+n_samples = 1000
+n_features = 10
 
-# Preprocess data
-...
+# Create time series data
+time_index = pd.date_range('2022-01-01', periods=n_samples, freq='H')
+sensor_data = np.random.rand(n_samples, n_features)
 
-# Split data
+# Create binary target indicating machine failure
+target = np.zeros(n_samples, dtype=int)
+failure_idx = np.random.randint(100, n_samples, size=5)
+target[failure_idx] = 1
+
+# Combine data into DataFrame
+data = pd.DataFrame(sensor_data, index=time_index, columns=[f'Sensor {i+1}' for i in range(n_features)])
+data['Target'] = target
+
+# Split data into training and testing sets
+X = data.drop('Target', axis=1)
+y = data['Target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Train model
+# Train a random forest classifier
 model = RandomForestClassifier(n_estimators=100)
 model.fit(X_train, y_train)
 
-# Evaluate model
+# Evaluate the model
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print('Accuracy:', accuracy)
